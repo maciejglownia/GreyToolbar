ae81c7daa1440486c789dda6d415d4ccd578a422
Maciej Glownia <maciej.glownia@infor.com>
2022-10-24 16:17:51 +0200

    Implement vectors to be clickable and displaying Snackbar

--- app/src/main/java/com/glownia/maciej/greytoolbar/MainActivity.kt
+++ app/src/main/java/com/glownia/maciej/greytoolbar/MainActivity.kt
@@ -2,10 +2,33 @@ package com.glownia.maciej.greytoolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.glownia.maciej.greytoolbar.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.companyIconToolbarNineDotsMenu.setOnClickListener { view ->
+            Snackbar.make(view, "Nine dots menu clicked!", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        binding.companyIconToolbarCompanyIcon.setOnClickListener { view ->
+            Snackbar.make(view, "Android icon clicked!", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        binding.companyIconToolbarMagnifier.setOnClickListener { view ->
+            Snackbar.make(view, "Magnifier clicked!", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        binding.companyIconToolbarDoubleLeftArrows.setOnClickListener { view ->
+            Snackbar.make(view, "Double left arrows clicked!", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
 }
